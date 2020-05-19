f94badf6a9e8ea4dcb282e0091c9051dbec1cfd0
Juan Manuel Kelsy Romero <juan.kelsy@globant.com>
2020-05-19 13:38:03 -0500

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private readonly DataContext _context;
+    public ValuesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var values = await _context.Values.ToListAsync();        
+        return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetValue(int id){
+        var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+        return Ok(value);
+    }
+
+}
